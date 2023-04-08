294a650f0a2406fc942b2834226ec3b76f0fb099
Alexandru <barbulescualexandru93@gmail.com>
2023-04-08 14:43:55 +0300

    minor changes

--- selenium_test.py
+++ selenium_test.py
@@ -4,22 +4,27 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Firefox()
+driver.maximize_window()
 driver.implicitly_wait(5)
 driver.get('https://www.google.com')
 driver.implicitly_wait(5)
-respinge_tot = driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/div[3]/span/div/div/div/div[3]/div[1]/button[1]/div')         #reject all cookie settings
+respinge_tot = driver.find_element(By.ID,'W0wltc')         #reject all cookie settings
 respinge_tot.click()
 driver.implicitly_wait(3)
 cautare_rtx = driver.find_element(By.NAME, 'q')
-cautare_rtx.send_keys('RTX 3060 TI' + Keys.ENTER)           #search for gpu
-driver.implicitly_wait(2)
-driver.find_element(By.XPATH, '/html/body/div[7]/div/div[11]/div[1]/div[2]/div[2]/div/div/div[2]/div/div/div[1]/div/a/h3').click()      #go to compari
+cautare_rtx.click()
+cautare_rtx.send_keys("RTX 3060 TI" + Keys.ENTER)
+driver.implicitly_wait(5)
+compari = driver.find_element(By.XPATH, "/html/body/div[7]/div/div[11]/div[1]/div[2]/div[2]/div/div/div[2]/div/div/div[1]/div/a/h3")
+compari.click()
 driver.implicitly_wait(10)
-driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[2]/div[2]/div[2]/div[3]/div[10]/div[1]/div[1]/div[1]').click()
+driver.find_element(By.CLASS_NAME, 'dismiss').click()  #got an error:element is not clickable at point beacuse another element obscures it.Workaround needed
+vision_oc = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[2]/div[2]/div[2]/div[3]/div[4]/div[1]/div[2]/div[1]/h2/a')
+vision_oc.click()
 driver.implicitly_wait(3)
 driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[2]/div[5]/div/div/div[1]/div[3]/div[1]/div[1]/div[7]/div[1]').click()
-driver.implicitly_wait(10)
-accept_cookies = driver.find_element(By.XPATH, '/html/body/div[7]/div/div/div[2]/div/div[2]')
-accept_cookies.click()
+driver.implicitly_wait(15)
