9c5ee98b812d3adc9bb93034137b4605d1d30158
Alexandru9s <barbulescualexandru93@gmail.com>
2023-04-10 15:24:12 +0300

    learned things about locators -made some changes

--- selenium_test.py
+++ selenium_test.py
@@ -13,18 +13,18 @@ driver.implicitly_wait(5)
 driver.get('https://www.google.com')
 driver.implicitly_wait(5)
 respinge_tot = driver.find_element(By.ID,'W0wltc')         #reject all cookie settings
-respinge_tot.click()
+respinge_tot.click()                                        
 driver.implicitly_wait(3)
 cautare_rtx = driver.find_element(By.NAME, 'q')
 cautare_rtx.click()
-cautare_rtx.send_keys("RTX 3060 TI" + Keys.ENTER)
+cautare_rtx.send_keys("RTX 3060 TI" + Keys.ENTER)           #search for GPU on google
 driver.implicitly_wait(5)
-compari = driver.find_element(By.XPATH, "/html/body/div[7]/div/div[11]/div[1]/div[2]/div[2]/div/div/div[2]/div/div/div[1]/div/a/h3")
+compari = driver.find_element(By.PARTIAL_LINK_TEXT, "Compari.ro")   #go to compari
 compari.click()
 driver.implicitly_wait(10)
 driver.find_element(By.CLASS_NAME, 'dismiss').click()  #got an error:element is not clickable at point beacuse another element obscures it.Workaround needed
-vision_oc = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[2]/div[2]/div[2]/div[3]/div[4]/div[1]/div[2]/div[1]/h2/a')
+vision_oc = driver.find_element(By.PARTIAL_LINK_TEXT, 'GIGABYTE GeForce RTX 3060 Ti VISION OC')
 vision_oc.click()
 driver.implicitly_wait(3)
-driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[2]/div[5]/div/div/div[1]/div[3]/div[1]/div[1]/div[7]/div[1]').click()
+driver.find_element(By.CLASS_NAME, 'fjump').click()
 driver.implicitly_wait(15)
